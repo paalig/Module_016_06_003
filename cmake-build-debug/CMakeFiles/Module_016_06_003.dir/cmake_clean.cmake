efd21cc347df66f1bcbd29193377b3cda67b37b9
Alex <alex0692pash@mail.ru>
2020-10-31 23:40:28 +0300

    Init

---
+++ cmake-build-debug/CMakeFiles/Module_016_06_003.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module_016_06_003.dir/main.cpp.obj"
+  "Module_016_06_003.exe"
+  "Module_016_06_003.exe.manifest"
+  "Module_016_06_003.pdb"
+  "libModule_016_06_003.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Module_016_06_003.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Module_016_06_003.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Module_016_06_003.
+# This may be replaced when dependencies are built.
